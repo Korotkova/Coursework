ed52be86b440266e617815a637654b7bea7ecc65
Korotkova <katya11.05.1996@gmail.com>
2017-02-03 16:39:28 +0900

    NorthwestCorner

--- Application/src/application/NorthwestCorner.java
+++ Application/src/application/NorthwestCorner.java
@@ -1,5 +1,18 @@
 package application;
 
+import java.awt.Font;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.swing.table.DefaultTableModel;
+
 public final class NorthwestCorner {
     
     Integer rows, columns;
@@ -113,11 +126,9 @@ public final class NorthwestCorner {
                     }
                     if (ew.masU[i] == 999999 & k != i & ew.masV[j] != 999999) {
                         ew.masU[i] = ew.money[i][j] - ew.masV[j];
-                        //System.out.println("U[" + (i + 1) + "]=" + moneyMN[i][j] + "-" + masV[j] + "=" + masU[i]);
                     }
                     if (ew.masV[j] == 999999 & k != i & ew.masU[i] != 999999) {
                         ew.masV[j] = ew.money[i][j] - ew.masU[i];
-                        //System.out.println("V[" + (j + 1) + "]=" + moneyMN[i][j] + "-" + masU[i] + "=" + masV[j]);
                     }
                 }
             }
@@ -132,7 +143,7 @@ public final class NorthwestCorner {
             }
         }
        System.out.println("Оптимальное решение Z = " + Z1 + " ус. ед.");
-       Z=Z1;
+       Z = Z1;
         flagfirstX = false;
         for (int i = 0; i < rows; i++) {
             System.out.println("U[" + (i + 1) + "] = " + ew.masU[i]);
@@ -492,6 +503,237 @@ public final class NorthwestCorner {
             System.out.println();
         }
         System.out.println("Z = " + Z + " ус. ед.");
+        
+        
+        
+        
+        JFrame jf = new JFrame();
+        jf.setTitle("Решение Метода Северо-Западного угла");
+        jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jf.setResizable(false);
+        jf.setBounds(300, 200, 1000, 300);
+        
+        JPanel panel = new JPanel();
+        JScrollPane jScrollPane1 = new JScrollPane();
+        JScrollPane jScrollPane2 = new JScrollPane();
+        JScrollPane jScrollPane3 = new JScrollPane();
+        JScrollPane jScrollPane4 = new JScrollPane();
+        JTable table1 = new JTable();
+        JTable table2 = new JTable();
+        JTable table3 = new JTable();
+        JTable table4 = new JTable();
+        JLabel label1 = new JLabel();
+        JLabel label2 = new JLabel();
+        JLabel label3 = new JLabel();
+        JLabel label4 = new JLabel();
+        JLabel label5 = new JLabel();
+        JLabel label6 = new JLabel();
+        JLabel label7 = new JLabel();
+        JTextField jTextField1 = new JTextField();
+        JTextField jTextField2 = new JTextField();
+        JTextField jTextField3 = new JTextField();
+        JTextField jTextField4 = new JTextField();
+        JButton button = new JButton();
+
+        table1.setFont(new Font("Tahoma", 0, 14)); 
+        table1.setModel(new DefaultTableModel(new Object [rows+1][columns+1], new String [] {"1", "2", "3", "4", "5", "6", "7"}) {
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                    return Integer.class;
+            }
+        });
+        table1.setRowHeight(20);
+        table1.getTableHeader().setResizingAllowed(false);
+        table1.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(table1);
+
+        label1.setText("Предложение");
+
+        label2.setFont(new Font("Tahoma", 0, 12));
+        label2.setText("Спрос");
+
+        label3.setText("Задача сбалансированна:");
+
+        jTextField1.setFont(new Font("Tahoma", 0, 12));
+        jTextField1.setText("jTextField1");
+
+        jTextField2.setFont(new Font("Tahoma", 0, 12));
+        jTextField2.setText("jTextField2");
+
+        label4.setFont(new Font("Tahoma", 1, 12));
+        label4.setText("Опорный план:");
+
+        table2.setModel(new DefaultTableModel(new Object[rows+1][columns+1], new String [] {"1", "2", "3", "4", "5", "6", "7"}) {
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                    return Integer.class;
+            }
+        });
+        table2.setRowHeight(20);
+        jScrollPane2.setViewportView(table2);
+
+        label5.setFont(new Font("Tahoma", 0, 14));
+        label5.setText("ЦФ:");
+
+        jTextField3.setFont(new Font("Tahoma", 0, 12));
+        jTextField3.setText("jTextField3");
+
+        label6.setFont(new Font("Tahoma", 1, 12));
+        label6.setText("Метод потенциалов:");
+
+        table3.setModel(new DefaultTableModel(new Object [vonI][2], new String [] {"НеБП", "Ui + Vj - Cij"}) {
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                    return Integer.class;
+            }
+        });
+        table3.setRowHeight(20);
+        jScrollPane3.setViewportView(table3);
+
+        table4.setModel(new DefaultTableModel(new Object [kI][3], new String [] {"БП", "Урав. относ. потенц.", "Решение"}) {
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                    return Integer.class;
+            }
+        });
+        table4.setRowHeight(20);
+        jScrollPane4.setViewportView(table4);
+        if (table4.getColumnModel().getColumnCount() > 0) {
+            table4.getColumnModel().getColumn(2).setHeaderValue("Решение");
+        }
+
+        label7.setFont(new Font("Tahoma", 1, 12));
+        label7.setText("Наибольшее значение");
+
+        jTextField4.setFont(new Font("Tahoma", 0, 12));
+        jTextField4.setText("jTextField4");
+
+        button.setFont(new Font("Tahoma", 0, 14));
+        button.setText("Следующий шаг");
+
+        GroupLayout jPanel1Layout = new GroupLayout(panel);
+        panel.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(label4)
+                        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(label2)
+                        .addGap(11, 11, 11)
+                        .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 371, GroupLayout.PREFERRED_SIZE)
+                        .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(label3)
+                                        .addGap(0, 0, Short.MAX_VALUE))
+                                    .addComponent(jTextField1))
+                                .addContainerGap())
+                            .addGroup(GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jTextField2, GroupLayout.PREFERRED_SIZE, 83, GroupLayout.PREFERRED_SIZE)
+                                .addGap(95, 95, 95))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                    .addComponent(label6)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jScrollPane2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                        .addGap(37, 37, 37)
+                                        .addComponent(label5)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jTextField3, GroupLayout.PREFERRED_SIZE, 115, GroupLayout.PREFERRED_SIZE)))
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jScrollPane4, GroupLayout.PREFERRED_SIZE, 256, GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(label7)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(jTextField4, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)))
+                                .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(58, 58, 58)
+                                        .addComponent(jScrollPane3, GroupLayout.PREFERRED_SIZE, 256, GroupLayout.PREFERRED_SIZE)
+                                        .addContainerGap(115, Short.MAX_VALUE))
+                                    .addGroup(GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(button)
+                                        .addContainerGap())))))
+                    .addGroup(GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(label1)
+                        .addGap(282, 282, 282))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(label1)
+                        .addGap(9, 9, 9)
+                        .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(label3)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 238, GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(249, 249, 249)
+                        .addComponent(label2)))
+                .addGap(18, 18, 18)
+                .addComponent(label4)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, GroupLayout.PREFERRED_SIZE, 238, GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(43, 43, 43)
+                        .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                            .addComponent(label5)
+                            .addComponent(jTextField3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(label6)
+                .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane3, GroupLayout.PREFERRED_SIZE, 247, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane4, GroupLayout.PREFERRED_SIZE, 247, GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                            .addComponent(label7)
+                            .addComponent(jTextField4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(40, Short.MAX_VALUE))
+                    .addGroup(GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(button, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())))
+        );
+
+        GroupLayout layout = new GroupLayout(jf.getContentPane());
+        jf.getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(panel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 54, Short.MAX_VALUE))
+        );
+
+        jf.pack();
+        jf.setVisible(true);
     }
 }
-
